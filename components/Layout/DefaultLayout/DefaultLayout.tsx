b52c12539beb9bc2902e22a4d41049cb782110c5
persol-hoangtruong <hoang.truong@x-factory.jp>
2023-01-04 16:43:40 +0700

    Feat: handle private route

--- components/Layout/DefaultLayout/DefaultLayout.tsx
+++ components/Layout/DefaultLayout/DefaultLayout.tsx
@@ -1,7 +1,9 @@
 import { css } from "@emotion/css";
+import { useRouter } from "next/router";
 import React from "react";
 
 import Header from "~@/components/Layout/Header/Header";
+import { useAuth } from "~@/contexts/authContext";
 
 
 export declare interface DefaultLayoutProps {
@@ -9,13 +11,35 @@ export declare interface DefaultLayoutProps {
 }
 
 export default function DefaultLayout({ children }: DefaultLayoutProps) {
+  const router = useRouter();
+  const { user } = useAuth();
 
-  return (
+  const isLoginPage = router.asPath === "/login";
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+
+  React.useEffect(() => {
+    if (!user) {
+      router.push("/login");
+      return;
+    }
+
+    setIsAuthenticated(true);
+  }, [router, user]);
+
+  if (isLoginPage) {
+    return (
+      <div className={layoutCss}>
+        {children}
+      </div>
+    );
+  }
+
+  return isAuthenticated ? (
     <div className={layoutCss}>
       <Header />
       {children}
     </div>
-  );
+  ) : null;
 }
 
 // ===========================================================================
